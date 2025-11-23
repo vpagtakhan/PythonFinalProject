24f8997ffebfd6d6d10693bd97ff2347076d5bb0
Noah Pagtakhan <vpagtakhan@academic.rrc.ca>
2025-11-22 23:20:59 -0600

    completed miletsone2

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request, send_file
 from db_operations import DBOperations
+from scrape_weather import WeatherScraper
+from plot_operations import PlotOperations
 import io
 import csv
 import json
+import matplotlib
+matplotlib.use("Agg")
 
 db = DBOperations(db_name="weather.db")
+scraper = WeatherScraper()
+plotter = PlotOperations("weather.db")
 app = Flask(__name__)
 
+url_template = (
+        "https://climate.weather.gc.ca/climate_data/daily_data_e.html?"
+        "StationID={sid}&timeframe=2&StartYear=1840&EndYear={year}&Day=1&Year={year}&Month={month}"
+    )
 
 @app.route('/')
 def home():
@@ -65,6 +75,60 @@ def generate_json(rows, start_year: int, end_year: int):
         as_attachment=True,
         download_name=f"weather_name_{start_year}_{end_year}.json",
     )
+
+@app.route("/update_data")
+def update_data():
+    scraped_data = scraper.scrape_back_in_time(url_template, station_id=27174, max_months=1)
+
+    if not scraped_data:
+        return "No new data has been returned from Environment Canada.", 500
+    
+    db.save_weather_data("Winnipeg", scraped_data)
+    
+    return f"Weather data updated. {len(scraped_data)} daily records have been saved."
+
+@app.route("/plot_box")
+def plot_box():
+    start_year = request.args.get("start_year")
+    end_year = request.args.get("end_year")
+
+    if not start_year or not end_year:
+        return "Missing start and end year", 400
+    
+    try:
+        start_year = int(start_year)
+        end_year = int(end_year)
+    except ValueError:
+        return "Invalid year values", 400
+    
+    buf = plotter.box_to_png(start_year, end_year)
+    if buf is None:
+        return f"No data found for {start_year}-{end_year}.", 404
+    
+    return send_file(buf, mimetype="image/png")
+
+@app.route("/plot_line")
+def plot_line():
+    year = request.args.get("year")
+    month = request.args.get("month")
+
+    if not year or not month:
+        return "Missing year or month", 400
+    
+    try:
+        year = int(year)
+        month = int(month)
+    except ValueError:
+        return "Invalid year or month", 400
+    
+    if month < 1 or month > 12:
+        return "Mont must be between 1 and 12", 400
+    
+    buf = plotter.line_to_png(year, month)
+    if buf is None:
+        return f"No data found for {year}-{month:02d}", 404
+    
+    return send_file(buf, mimetype="image/png")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- plot_operations.py
+++ plot_operations.py
@@ -1,5 +1,9 @@
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
+import io
 from db_operations import DBOperations
+
 
 class PlotOperations:
     """class that allows data from DBOperations to be used for creating box plot and line plot graphs"""
@@ -41,6 +45,41 @@ class PlotOperations:
         plt.grid(True, linestyle='--', alpha=0.5)
         plt.show()
 
+    def box_to_png(self, start_year: int, end_year: int):
+
+        year_to_temps = {}
+
+        for year in range(start_year, end_year +1):
+            monthly_data = self.db.fetch_mean_month(year)
+
+            all_temps = []
+            for month, temps in monthly_data.items():
+                all_temps.extend([t for t in temps if t is not None])
+            
+            if all_temps:
+                year_to_temps[year] = all_temps
+        
+        if not year_to_temps:
+            print("No temperature data found in this range.")
+            return
+        
+        data_for_plot = list(year_to_temps.values())
+        labels = list(year_to_temps.keys())
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.boxplot(data_for_plot)
+        ax.set_xticklabels(labels)
+        ax.set_title(f"Mean Temperatures for date range: {start_year}-{end_year}")
+        ax.set_ylabel("Mean Temperature")
+        ax.grid(True, linestyle="--", alpha=0.5)
+
+        buf = io.BytesIO()
+        fig.savefig(buf, format="png", bbox_inches="tight")
+        buf.seek(0)
+        plt.close(fig)
+
+        return buf
+
     def line_plot_mean_temps(self, year: int, month: int):
         """Uses a line plot to display daily average temps for a whole month withing a specifc year"""
 
@@ -61,3 +100,29 @@ class PlotOperations:
         plt.ylabel("Mean Temperature")
         plt.grid(True, linestyle="--", alpha=0.5)
         plt.show()
+
+    def line_to_png(self, year: int, month: int):
+
+        monthly_data = self.db.fetch_mean_month(year)
+
+        if month not in monthly_data:
+            print(f"No data found for year {year}-{month:02d}")
+            return
+        
+        """preparing the data to be used in plotting"""
+        temps = monthly_data[month]
+        days = list(range(1, len(temps) + 1))
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.plot(days, temps, marker='o')
+        ax.set_title(f"Daily Mean Temperatures - {year}-{month:02d}")
+        ax.set_xlabel("Day")
+        ax.set_ylabel("Mean Temperatures")
+        ax.grid(True, linestyle="--", alpha=0.5)
+
+        buf = io.BytesIO()
+        fig.savefig(buf, format="png", bbox_inches="tight")
+        buf.seek(0)
+        plt.close(fig)
+
+        return buf
